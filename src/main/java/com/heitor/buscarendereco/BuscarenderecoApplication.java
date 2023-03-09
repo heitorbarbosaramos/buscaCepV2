875ce3f367b8b99b955d29c059c73dace4fc9b12
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2023-03-09 13:22:56 -0300

    Adcionando dependencia do Feign

--- src/main/java/com/heitor/buscarendereco/BuscarenderecoApplication.java
+++ src/main/java/com/heitor/buscarendereco/BuscarenderecoApplication.java
@@ -2,8 +2,10 @@ package com.heitor.buscarendereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BuscarenderecoApplication {
 
 	public static void main(String[] args) {
