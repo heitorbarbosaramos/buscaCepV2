e7f16f008010a603af38c691d6ea67ed89d2c3e1
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2023-03-09 11:53:13 -0300

    Projeto criado

---
+++ src/main/java/com/heitor/buscarendereco/BuscarenderecoApplication.java
@@ -0,0 +1,13 @@
+package com.heitor.buscarendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscarenderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscarenderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heitor/buscarendereco/BuscarenderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitor.buscarendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscarenderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
